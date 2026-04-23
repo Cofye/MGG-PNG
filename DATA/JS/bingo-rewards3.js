1ef6b554c0bf34386147860475ccc92e6c054dc0
Cofye <71459639+Cofye@users.noreply.github.com>
2026-04-23 04:26:37 -0500

    more hd's

---
+++ DATA/JS/bingo-rewards3.js
@@ -0,0 +1,79 @@
+window.bingoRewards = {
+  // Bingo Events 2019
+  "event2019l1": { amount: "x50", id: "Material_Jackpot_Token" },
+  "event2019l2": { amount: "x6", id: "Material_Gacha_Token" },
+  "event2019l3": { amount: "x1500", id: "hardcurrency" },
+  "event2019c1": { amount: "x4", id: "Star_Gold" },
+  "event2019c2": { amount: "x2", id: "Star_Platinum" },
+  "event2019c3": { amount: "x1500", id: "hardcurrency" },
+  "event2019s1": { amount: "x3000", id: "hardcurrency" },
+
+  // Bingo Events 2020
+  "event2020l1": { amount: "x2", id: "Star_Platinum" },
+  "event2020l2": { amount: "x30", id: "Material_Event_Token" },
+  "event2020l3": { amount: "x6", id: "Material_Gacha_Token" },
+  "event2020l4": { amount: "x1200", id: "hardcurrency" },
+  "event2020c1": { amount: "x75", id: "Material_Jackpot_Token" },
+  "event2020c2": { amount: "x4", id: "Star_Gold" },
+  "event2020c3": { amount: "x1800", id: "hardcurrency" },
+  "event2020s1": { amount: "x3000", id: "hardcurrency" },
+
+  // Bingo Events 2021
+  "event2021l1": { amount: "x2", id: "Star_Platinum" },
+  "event2021l2": { amount: "x6", id: "Material_Gacha_Token" },
+  "event2021l3": { amount: "x30", id: "Material_Event_Token" },
+  "event2021l4": { amount: "x1200", id: "hardcurrency" },
+  "event2021c1": { amount: "x75", id: "Material_Jackpot_Token" },
+  "event2021c2": { amount: "x4", id: "Star_Gold" },
+  "event2021c3": { amount: "x1800", id: "hardcurrency" },
+  "event2021c4": { amount: "x100", id: "Material_XP1000" },
+  "event2021s1": { amount: "x3000", id: "hardcurrency" },
+
+  // Bingo Events 2022
+  "event2022l1": { amount: "x30", id: "Material_Event_Token" },
+  "event2022l2": { amount: "x6", id: "Material_Gacha_Token" },
+  "event2022l3": { amount: "x1200", id: "hardcurrency" },
+  "event2022l4": { amount: "x2", id: "Star_Platinum" },
+  "event2022c1": { amount: "x100", id: "Material_XP1000" },
+  "event2022c2": { amount: "x75", id: "Material_Jackpot_Token" },
+  "event2022c3": { amount: "x1800", id: "hardcurrency" },
+  "event2022c4": { amount: "x4", id: "Star_Gold" },
+  "event2022s1": { amount: "x3000", id: "hardcurrency" },
+
+  // Bingo Events 2024
+  "event2024l1": { amount: "x75", id: "Material_Jackpot_Token" },
+  "event2024l2": { amount: "x4", id: "Star_Gold" },
+  "event2024l3": { amount: "x1600", id: "hardcurrency" },
+  "event2024l4": { amount: "x6", id: "Material_Gacha_Token" },
+  "event2024c1": { amount: "x40", id: "Material_XP1000" },
+  "event2024c2": { amount: "x1400", id: "hardcurrency" },
+  "event2024c3": { amount: "x30", id: "Material_Event_Token" },
+  "event2024c4": { amount: "x2", id: "Star_Platinum" },
+  "event2024s1": { amount: "x3000", id: "hardcurrency" },
+
+  // Bingo Events 2025
+  "Events202501": { amount: "x2", id: "Material_Gacha_Token" },
+  "Events202502": { amount: "x5", id: "Star_Silver" },
+  "Events202503": { amount: "x2", id: "orb_basic_life_06" },
+  "Events202504": { amount: "x500", id: "hardcurrency" },
+  "Events202505": { amount: " ", id: "Star_Platinum" },
+  "Events202506": { amount: "x15", id: "Material_Event_Token" },
+  "Events202507": { amount: " ", id: "Star_Gold" },
+  "Events202508": { amount: "x40", id: "Material_Jackpot_Token" },
+  "Events202509": { amount: "x500", id: "hardcurrency" },
+  "Events202510": { amount: "x5", id: "Material_Mystery25_Token" },
+  "Events202511": { amount: "x2000", id: "hardcurrency" },
+
+  // Bingo Events 2026
+  "Events202601": { amount: "x2", id: "Material_Gacha_Token" },
+  "Events202602": { amount: "x5", id: "Star_Silver" },
+  "Events202603": { amount: "x2", id: "orb_basic_life_06" },
+  "Events202604": { amount: "x500", id: "hardcurrency" },
+  "Events202605": { amount: " ", id: "Star_Platinum" },
+  "Events202606": { amount: "x15", id: "Material_Event_Token" },
+  "Events202607": { amount: " ", id: "Star_Gold" },
+  "Events202608": { amount: "x40", id: "Material_Jackpot_Token" },
+  "Events202609": { amount: "x500", id: "hardcurrency" },
+  "Events202610": { amount: "x5", id: "Material_Mystery26_Token" },
+  "Events202611": { amount: "x2000", id: "hardcurrency" }
+};
---
+++ DATA/JS/bingo-rewards4.js
@@ -0,0 +1,53 @@
+window.bingoRewards = {
+  // Bingo Mutants 2025
+  "Mutants202501": { amount: "x1100", id: "hardcurrency" },
+  "Mutants202502": { amount: "x50", id: "Material_XP1000" },
+  "Mutants202503": { amount: "x5", id: "Star_Silver" },
+  "Mutants202504": { amount: "x2", id: "Star_Gold" },
+  "Mutants202505": { amount: "x2", id: "Star_Platinum" },
+  "Mutants202506": { amount: "x1100", id: "hardcurrency" },
+  "Mutants202507": { amount: "x3", id: "Material_Gacha_Token" },
+  "Mutants202508": { amount: "x20", id: "Material_Event_Token" },
+  "Mutants202509": { amount: "x75", id: "Material_Jackpot_Token" },
+  "Mutants202510": { amount: "x2", id: "orb_basic_life_06" },
+  "Mutants202511": { amount: "x3800", id: "hardcurrency" },
+
+  // Bingo Mutants 2026
+  "Mutants202601": { amount: "x1100", id: "hardcurrency" },
+  "Mutants202602": { amount: "x50", id: "Material_XP1000" },
+  "Mutants202603": { amount: "x5", id: "Star_Silver" },
+  "Mutants202604": { amount: "x2", id: "Star_Gold" },
+  "Mutants202605": { amount: "x2", id: "Star_Platinum" },
+  "Mutants202606": { amount: "x1100", id: "hardcurrency" },
+  "Mutants202607": { amount: "x3", id: "Material_Gacha_Token" },
+  "Mutants202608": { amount: "x20", id: "Material_Event_Token" },
+  "Mutants202609": { amount: "x75", id: "Material_Jackpot_Token" },
+  "Mutants202610": { amount: "x2", id: "orb_basic_life_06" },
+  "Mutants202611": { amount: "x3800", id: "hardcurrency" },
+
+  // Bingo Skins 2025
+  "Skins202501": { amount: "x50", id: "Material_Jackpot_Token" },
+  "Skins202502": { amount: "x3", id: "orb_basic_xp_04" },
+  "Skins202503": { amount: "x1000", id: "hardcurrency" },
+  "Skins202504": { amount: "x2", id: "Material_Gacha_Token" },
+  "Skins202505": { amount: "x2", id: "Star_Platinum" },
+  "Skins202506": { amount: "x4", id: "Star_Silver" },
+  "Skins202507": { amount: "x20", id: "Material_Event_Token" },
+  "Skins202508": { amount: "x1000", id: "hardcurrency" },
+  "Skins202509": { amount: "x2", id: "orb_basic_attack_06" },
+  "Skins202510": { amount: "x2", id: "Star_Gold" },
+  "Skins202511": { amount: "x3200", id: "hardcurrency" },
+
+  // Bingo Skins 2026
+  "Skins202601": { amount: "x50", id: "Material_Jackpot_Token" },
+  "Skins202602": { amount: "x3", id: "orb_basic_xp_04" },
+  "Skins202603": { amount: "x1000", id: "hardcurrency" },
+  "Skins202604": { amount: "x2", id: "Material_Gacha_Token" },
+  "Skins202605": { amount: "x2", id: "Star_Platinum" },
+  "Skins202606": { amount: "x4", id: "Star_Silver" },
+  "Skins202607": { amount: "x20", id: "Material_Event_Token" },
+  "Skins202608": { amount: "x1000", id: "hardcurrency" },
+  "Skins202609": { amount: "x2", id: "orb_basic_attack_06" },
+  "Skins202610": { amount: "x2", id: "Star_Gold" },
+  "Skins202611": { amount: "x3200", id: "hardcurrency" }
+};
